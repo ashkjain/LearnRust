94592b24b911c32bed3c4caec7a27216ddbcbe4a
ashishjain-repo <ashishjain99@yahoo.com>
2024-04-24 17:50:14 -0600

    Added project name variables

---
+++ aOne/variables/target/debug/deps/variables-bbf64ff9810a0f30.d
@@ -0,0 +1,5 @@
+C:\Users\aj901\OneDrive\Learning\Rust\LearnRust\aOne\variables\target\debug\deps\variables-bbf64ff9810a0f30.exe: src\main.rs
+
+C:\Users\aj901\OneDrive\Learning\Rust\LearnRust\aOne\variables\target\debug\deps\variables-bbf64ff9810a0f30.d: src\main.rs
+
+src\main.rs:
---
+++ aOne/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\aj901\OneDrive\Learning\Rust\LearnRust\aOne\variables\target\debug\variables.exe: C:\Users\aj901\OneDrive\Learning\Rust\LearnRust\aOne\variables\src\main.rs
