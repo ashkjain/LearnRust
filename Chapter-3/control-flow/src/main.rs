c7929db07aa87a2517fa6c25253f19c63a138216
ashishjain-repo <ashishjain99@yahoo.com>
2024-04-26 09:49:21 -0600

    Completed control flow

--- Chapter-3/control-flow/src/main.rs
+++ Chapter-3/control-flow/src/main.rs
@@ -72,4 +72,62 @@ fn main() {
         }
     };
     println!("The result is {}",result);
+
+    // Loop labels to disambiguate between multuple loops
+
+    let mut count = 0;
+    'counting_up: loop
+    {
+        println!("count = {}",count);
+        let mut remaining = 10;
+
+        loop
+        {
+            println!("Remaining = {}",remaining);
+            if remaining == 9
+            {
+                break;
+            }
+            if count == 2
+            {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {}",count);
+
+    // Condition loops with while
+    let mut number = 3;
+
+    while number != 0
+    {
+        println!("{}!",number);
+        number -= 1;
+    }
+    println!("LIFTOFF!");
+
+    // Looping through a collection with for
+
+    let a = [10,20,30,40,50];
+    let mut index = 0;
+
+    while index < 5
+    {
+        println!("The value is: {}",a[index]);
+        index += 1;
+    }
+
+    for element in a
+    {
+        println!("The value is: {}",element);
+    }
+
+    // For loop with reverse numerical order function
+    for number in (1..4).rev()
+    {
+        println!("{}",number);
+    }
+    println!("LIFEOFF!");
 }
