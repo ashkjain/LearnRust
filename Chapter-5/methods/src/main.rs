03447817c4314fb554f004140f2df0de86a4cb48
ashishjain-repo <ashishjain99@yahoo.com>
2024-05-02 10:37:34 -0600

    Chapter-5 is finished with methods

--- Chapter-5/methods/src/main.rs
+++ Chapter-5/methods/src/main.rs
@@ -10,6 +10,28 @@ impl Rectangle
     {
         self.width * self.height
     }
+
+    fn width(&self) -> bool
+    {
+        self.width > 0
+    }
+
+    fn square(size: u32) -> Self
+    {
+        Self
+        {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle
+{
+    fn can_hold(&self, other: &Rectangle) -> bool
+    {
+        self.width > other.width && self.height > other.height
+    }
 }
 fn main()
 {
@@ -19,4 +41,37 @@ fn main()
         height: 50,
     };
     println!("The area of rectangle is {} square pixels.", rect1.area());
+
+    let rect2 = Rectangle
+    {
+        width: 30,
+        height: 50,
+    };
+
+    if rect2.width()
+    {
+        println!("The rectangle has a nonzero width, it is {}", rect2.width);
+    }
+
+    let rect3 = Rectangle
+    {
+        width: 30,
+        height: 50,
+    };
+    let rect4 = Rectangle
+    {
+        width: 10,
+        height: 40,
+    };
+    let rect5 = Rectangle
+    {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect3 hold rect4? {}",rect3.can_hold(&rect4));
+    println!("Can rect3 hold rect5? {}",rect3.can_hold(&rect5));
+
+    let sq = Rectangle::square(3);
+    println!("Square-Height: {}, Square-Width: {}",sq.height, sq.width);
 }
